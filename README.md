fe6355fef1a7e33c20c7c6eb02e635479576a740
Arthur <wang@xunnn.com>
2019-06-17 19:44:44 +0800

    数组方法(forEach)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aw-js-es6
 es6 demo
+
+#ES6定义的一些新方法以及和JavaScripot的一些区别整理。
---
+++ app.js
@@ -0,0 +1,37 @@
+//遍历数组
+
+    // var colors = ["red","blue","green"];
+
+    // //ES5遍历数组的方法
+    // for(var i=0; i<colors.length; i++){
+    //     console.log(colors[i]);
+    // }
+
+    // //ES6 forEach
+    // colors.forEach(function(color){
+    //     console.log(color);
+    // });
+
+    // colors.forEach((color) => {
+    //     console.log(color);
+    // })
+
+
+    
+
+    // //练习：遍历数组中的值，并计算总和
+    // var numbers = [1,2,3,4,5];
+    // var sum = 0;
+    // numbers.forEach((number) => {
+    //     sum += number;
+    // })
+    // console.log(sum);
+
+
+    // var numbers = [1,2,3,4,5];
+    // var sum = 0;
+    // function adder(number){
+    //     sum += number;
+    // }
+    // numbers.forEach(adder);  //没有调用函数，只是将函数放进去
+    // console.log(sum);
