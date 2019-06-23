6035c67bdc4ed94f8bdf01f01d4c028cff3dbe43
Arthur <wang@xunnn.com>
2019-06-23 22:43:38 +0800

    新的数据结构(set)

--- app.js
+++ app.js
@@ -1,57 +1,47 @@
 /**
- * 数据结构：map
- * 键值对；与对象不同的是键和值可以是任何类型
+ * 数据结构：set
+ * 集合：可以存储任何数据类型，并且是唯一的（不重复的值）
+ * 
  */
 
+const set1 = new Set();
 
-const map1 = new Map();
+//向set1中添加数据
+set1.add(100);
+set1.add("A String");
+set1.add({name:"Henry"});
+set1.add(true);
+set1.add(100); //无作用，不可以重复添加
 
-//设置key键
-const key1 = 'some string',
-      key2 = {},
-      key3 = function(){};
+console.log(set1);
 
-//为key设置value值
-map1.set(key1,'Value of key1');
-map1.set(key2,'Value of key2');
-map1.set(key3,'Value of key3');
+//向set2中添加数据
+const set2 = new Set([1,true,'string']);
+console.log(set2);
 
-//根据key获取对应的value值
-console.log(map1.get(key1));
-console.log(map1.get(key2));
-console.log(map1.get(key3));
+// 计算set1中的数据个数
+console.log(set1.size);
 
-//获取对应的value数量
-console.log(map1.size);
-console.log(typeof map1);
+//检查set中是否拥有对应的值
+console.log(set1.has(100));
+console.log(set1.has(50*2));  //可以用计算的形式
+console.log(set1.has({name:"Henry"})); //false
+console.log({name:"Henry"} === {name:"Henry"}); //匹配的不是内容，是地址
 
-//for...of遍历map1中的key and value
-for(let [key,value] of map1){
-    console.log(`${key} = ${value}`);
+//删除set中的内容
+set1.delete(100);
+console.log(set1);
+
+//for..of 遍历set
+for(let item of set1){
+    console.log(item);
 }
 
-//只要key值
-for(let key of map1.keys()){
-    console.log(key);
-}
-//只要value值
-for(let value of map1.values()){
+//forEach遍历
+set1.forEach((value) => {
     console.log(value);
-}
-
-//forEach遍历map1
-map1.forEach((value,key) => {
-    console.log(`${key} = ${value}`);
 });
 
-//将map1转化为正常的数组
-const keyValueArray = Array.from(map1);
-console.log(keyValueArray);
-
-//将map1中的key转化为数组
-const keyArray = Array.from(map1.keys());
-console.log(keyArray);
-
-//将map1中的value值转换为数组
-const valueArray = Array.from(map1.values());
-console.log(valueArray);
+//将set转化为array
+const setArray = Array.from(set1);
+console.log(setArray);
