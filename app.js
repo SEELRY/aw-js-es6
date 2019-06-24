6f55aea85650845a0620d16928fab96f2ab689bb
Arthur <wang@xunnn.com>
2019-06-25 06:57:18 +0800

    Promise及fetch

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 /**
- * 数据结构：set
- * 集合：可以存储任何数据类型，并且是唯一的（不重复的值）
+ * 构造函数：Promise
+ * 钩子函数
  * 
  */
 
-const set1 = new Set();
+// let promise = new Promise();
+// console.log(promise);   //报错，not a function
 
-//向set1中添加数据
-set1.add(100);
-set1.add("A String");
-set1.add({name:"Henry"});
-set1.add(true);
-set1.add(100); //无作用，不可以重复添加
 
-console.log(set1);
 
-//向set2中添加数据
-const set2 = new Set([1,true,'string']);
-console.log(set2);
 
-// 计算set1中的数据个数
-console.log(set1.size);
+// let promise = new Promise((resolve,reject) => {
+//     resolve();
+//     // reject();
+// });
+// // console.log(promise);
 
-//检查set中是否拥有对应的值
-console.log(set1.has(100));
-console.log(set1.has(50*2));  //可以用计算的形式
-console.log(set1.has({name:"Henry"})); //false
-console.log({name:"Henry"} === {name:"Henry"}); //匹配的不是内容，是地址
+// promise
+// .then(() => {console.log("成功，没有任何问题")})
+// .then(() => {console.log("可以，无限调用多个then方法")})
+// .catch(() => {console.log("出现错误！")});
 
-//删除set中的内容
-set1.delete(100);
-console.log(set1);
 
-//for..of 遍历set
-for(let item of set1){
-    console.log(item);
-}
 
-//forEach遍历
-set1.forEach((value) => {
-    console.log(value);
-});
 
-//将set转化为array
-const setArray = Array.from(set1);
-console.log(setArray);
+/**
+ * fetch方法
+ */
+
+// console.log(window);
+// console.log(fetch);
+
+// http://jsonplaceholder.typicode.com/
+let url = "http://jsonplaceholder.typicode.com/posts";
+
+// console.log(fetch(url));
+fetch(url)
+    // .then(data => {console.log(data)});
+    .then(response => response.json())  //解析成json
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
