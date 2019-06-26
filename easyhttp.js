53a200b7e400e8673955afa3e59ab6e2b2db9eb4
Arthur <wang@xunnn.com>
2019-06-26 22:27:16 +0800

    async封装fetch

--- easyhttp.js
+++ easyhttp.js
@@ -2,59 +2,55 @@
  * 封装fetch
  * 更快，更简单的请求数据
  * 
+ * @version 2.0.0
  * 
  */
 
 class EasyHttp{
     //get封装
-    get(url){
-        return new Promise((resolve,reject) => {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err))
-        })
+    async get(url){
+            const response = await fetch(url);
+            const resData = await response.json();
+            return resData;
     }
+    
 
     //post封装
-    post(url,data) {
-        return new Promise((resolve, reject) => {
-            fetch(url,{
-                method:"POST",
-                headers:{'Content-type':'application/json'},
-                body:JSON.stringify(data)
-            })
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err))
-        })
+    async post(url, data) {
+        const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            const resData = await response.json();
+            return resData;
     }
 
     //put 更新数据
-    put(url,data){
-        return new Promise((resolve,reject) => {
-            fetch(url,{
-                method:"PUT",
-                headers:{'Content-type':'application/json'},
-                body:JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err))
-        })
+    async put(url,data){
+        const response = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            const resData = await response.json();
+            return resData;
     }
 
     //delete 删除数据
-    delete(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url,{
-                method:"DELETE",
-                headers:{'Content-type':'application/json'}
-            })
-                .then(res => res.json())
-                .then(data => resolve("数据删除成功！"))
-                .catch(err => reject(err))
-        })
+    async delete(url) {
+        const response = await fetch(url, {
+                method: "DELETE",
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            });
+            const resData = await "数据删除成功！"
+            return resData;
     }
 
 }
